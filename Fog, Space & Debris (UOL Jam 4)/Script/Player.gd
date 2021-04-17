0877cb0ee8e8933658160f6b4963af03ab2c20ff
Lennart Zurmühl <lennart@lennart-thinkpad.fritz.box>
2021-04-17 10:58:01 +0200

    Player movement

--- Fog, Space & Debris (UOL Jam 4)/Script/Player.gd
+++ Fog, Space & Debris (UOL Jam 4)/Script/Player.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Declare member variables here:
+var xDirection = 0
+var yDirection = 0
+const SPEED = 2
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,5 +14,28 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(_delta):
+	movement()
+	
+	
+
+
+
+func movement():
+	xDirection = 0
+	yDirection = 0
+	if Input.is_action_pressed("left") && self.position.x > 210:
+		xDirection -= 1
+	if Input.is_action_pressed("right") && self.position.x < 430:
+		xDirection += 1
+	if Input.is_action_pressed("down") && self.position.y < 290:
+		yDirection += 1
+	if Input.is_action_pressed("up") && self.position.y > 250:
+		yDirection -= 1
+	
+	self.position.x += xDirection * SPEED
+	self.position.y += yDirection * SPEED
+	
+
+
+
