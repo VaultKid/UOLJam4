2ff7ae18c65459c71e647a0739ef5f52750807a2
Keno <avaultkid@gmail.com>
2021-04-17 16:13:55 +0200

    audio manager

---
+++ Fog, Space & Debris (UOL Jam 4)/Script/AudioManager.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var volume = -6.0
+
+func _ready():
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), volume)
+	pass # Replace with function body.
+
+func playExplosion():
+	$"ExplosionSFX".play(0.0)
+	pass
+
+func _on_Intro_Player_finished():
+	$"Loop Player".play(0.0)
+	pass # Replace with function body.
