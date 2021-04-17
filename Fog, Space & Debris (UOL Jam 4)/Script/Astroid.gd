6dc0adda82783af4f9eabf52e45e29cb04ab5afd
Lennart Zurmühl <lennart@lennart-thinkpad.fritz.box>
2021-04-17 15:18:41 +0200

    added Rockets, Satellites and Asteroiden2

--- Fog, Space & Debris (UOL Jam 4)/Script/Astroid.gd
+++ Fog, Space & Debris (UOL Jam 4)/Script/Astroid.gd
@@ -4,16 +4,17 @@ extends Area2D
 # Declare member variables here. Examples:
 # var a = 2
 var is_alive = true #Ob der Asteroid fest ist und noch zerstoert werden kann
-const SPEED = 2 #Fluggeschwindigkeit des Asteroids
-
+var speed = 2 #Fluggeschwindigkeit des Asteroids
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+		
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	position.y += SPEED
+	position.y += speed
 	if position.y > 320: #loesche Asteroid, wenn er außerhalb des Bildschirms ist
 		queue_free()
 
--- Fog, Space & Debris (UOL Jam 4)/Script/Game.gd
+++ Fog, Space & Debris (UOL Jam 4)/Script/Game.gd
@@ -14,6 +14,9 @@ func _ready():
 func _process(_delta):
 	if spawnRdy: #Zähle den Cooldown bis der nächste Asteroid gespawnt ist
 		spawnAsteroid() 
+		spawnRocket()
+		spawnAsteroid2()
+		spawnSatellite()
 		currentCooldown = COOLDOWN
 		spawnRdy = false
 	elif currentCooldown == 0:
@@ -23,6 +26,24 @@ func _process(_delta):
 
 
 
+	
+
+func spawnSatellite():
+	rng.randomize()
+	var rngNumber = rng.randf_range(-120.0, 120.0) #Erzeuge zufaellige Position
+	var satellites = preload("res://Szenen/Satellite.tscn")
+	var satellite = satellites.instance() #spawn Rocket
+	satellite.set_position(Vector2(320 + rngNumber , -10))
+	self.add_child(satellite)
+
+func spawnRocket():
+	rng.randomize()
+	var rngNumber = rng.randf_range(-120.0, 120.0) #Erzeuge zufaellige Position
+	var rockets = preload("res://Szenen/Rocket.tscn")
+	var rocket = rockets.instance() #spawn Rocket
+	rocket.set_position(Vector2(320 + rngNumber , -10))
+	self.add_child(rocket)
+
 func spawnAsteroid():
 	rng.randomize()
 	var rngNumber = rng.randf_range(-120.0, 120.0) #Erzeuge zufaellige Position
@@ -30,4 +51,12 @@ func spawnAsteroid():
 	var asteroid = asteroids.instance() #spawn Asteroid
 	asteroid.set_position(Vector2(320 + rngNumber , -10))
 	self.add_child(asteroid)
+	
+func spawnAsteroid2():
+	rng.randomize()
+	var rngNumber = rng.randf_range(-120.0, 120.0) #Erzeuge zufaellige Position
+	var asteroids2 = preload("res://Szenen/Astroid2.tscn")
+	var asteroid2 = asteroids2.instance() #spawn Asteroid
+	asteroid2.set_position(Vector2(320 + rngNumber , -10))
+	self.add_child(asteroid2)
 	
