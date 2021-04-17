c06c164bc77a4eef9647f522b455391c93fc8baa
Lennart Zurmühl <lennart@lennart-thinkpad.fritz.box>
2021-04-17 16:21:07 +0200

    optimized spawning algorithm

--- Fog, Space & Debris (UOL Jam 4)/Script/Game.gd
+++ Fog, Space & Debris (UOL Jam 4)/Script/Game.gd
@@ -3,23 +3,37 @@ extends Node2D
 
 # Declare member variables here. Examples:
 var currentCooldown = 0 #aktuelle Ticks, bis der naechste Asteroid gespawnt wirds
-const COOLDOWN = 30 #Ticks zwisched dem Spawnen von Asteroiden
+var cooldown = 60 #Ticks zwisched dem Spawnen von Asteroiden
 var spawnRdy = true #Ob ein Asteroid gespawnt werden kann
 var rng = RandomNumberGenerator.new() 
+var spectrumStart = 0.5
+var spectrumEnd = 4.5
+const DIFFICULTY = 0.3
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	spectrumStart = 1.5 - (cooldown/60)
+	spectrumEnd = 5 - (cooldown/60)
 	if spawnRdy: #Zähle den Cooldown bis der nächste Asteroid gespawnt ist
-		spawnAsteroid() 
-		spawnRocket()
-		spawnAsteroid2()
-		spawnSatellite()
-		currentCooldown = COOLDOWN
+		rng.randomize()
+		var rngNumber = int(round(rng.randf_range(spectrumStart, spectrumEnd)))
+		if rngNumber <= 1:
+			spawnSatellite()
+		elif rngNumber == 2:
+			
+			spawnAsteroid() 
+		elif rngNumber == 3:
+			spawnAsteroid2()
+		else:
+			spawnRocket()
+		currentCooldown = cooldown
 		spawnRdy = false
-	elif currentCooldown == 0:
+		if cooldown > 10:
+			cooldown -= DIFFICULTY
+	elif currentCooldown <= 0:
 		spawnRdy = true
 	else:
 		currentCooldown -= 1
@@ -30,7 +44,7 @@ func _process(_delta):
 
 func spawnSatellite():
 	rng.randomize()
-	var rngNumber = rng.randf_range(-120.0, 120.0) #Erzeuge zufaellige Position
+	var rngNumber = rng.randf_range(-110.0, 110.0) #Erzeuge zufaellige Position
 	var satellites = preload("res://Szenen/Satellite.tscn")
 	var satellite = satellites.instance() #spawn Rocket
 	satellite.set_position(Vector2(320 + rngNumber , -10))
