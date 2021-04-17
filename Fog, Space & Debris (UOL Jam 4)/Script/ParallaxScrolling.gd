7e85d6620c65f80780c8899bf5c2cd5199522b2e
Keno <avaultkid@gmail.com>
2021-04-17 23:22:11 +0200

    delete game

--- Fog, Space & Debris (UOL Jam 4)/Script/ParallaxScrolling.gd
+++
@@ -1,9 +0,0 @@
-extends ParallaxBackground
-
-
-export (float) var scrolling_speed = 90.0
-
-func _process(delta):
-	scroll_offset.y += scrolling_speed * delta
-	$ParallaxLayer2.motion_offset.y += scrolling_speed * delta * 0.1
-	$ParallaxLayer3.motion_offset.y  += scrolling_speed * delta * 0.2
