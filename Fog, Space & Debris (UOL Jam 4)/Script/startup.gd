37ed5b697333f288b0be2ade2b15fc4e836a278a
Keno <avaultkid@gmail.com>
2021-04-18 17:33:01 +0200

    Project Pilot Audio

--- Fog, Space & Debris (UOL Jam 4)/Script/startup.gd
+++ Fog, Space & Debris (UOL Jam 4)/Script/startup.gd
@@ -8,7 +8,9 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	yield(get_tree().create_timer(2), "timeout")
+	yield(get_tree().create_timer(1), "timeout")
+	$"StartUpAudioPlayer".play(0.0)
+	yield(get_tree().create_timer(4.5), "timeout")
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"),  -14)
 	get_tree().change_scene("res://Szenen/Menu.tscn") 
 
