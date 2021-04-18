c5952a4e9f053cc3383ad1296d8083e4aa37890b
Lennart Zurmühl <lennart@lennart-thinkpad.fritz.box>
2021-04-18 17:04:17 +0200

    logo

--- Fog, Space & Debris (UOL Jam 4)/Script/startup.gd
+++ Fog, Space & Debris (UOL Jam 4)/Script/startup.gd
@@ -8,6 +8,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	yield(get_tree().create_timer(2), "timeout")
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"),  -14)
 	get_tree().change_scene("res://Szenen/Menu.tscn") 
 
