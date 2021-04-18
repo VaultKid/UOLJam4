66dca8eeb7a2a42b55214447b3820bcf2c3e567c
Keno <avaultkid@gmail.com>
2021-04-18 16:21:41 +0200

    Menu Music und Windows Export des 16:20 stands

--- Fog, Space & Debris (UOL Jam 4)/Script/Menu.gd
+++ Fog, Space & Debris (UOL Jam 4)/Script/Menu.gd
@@ -13,8 +13,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(str(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))))
-
+	#print(str(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))))
+	pass
 
 
 
