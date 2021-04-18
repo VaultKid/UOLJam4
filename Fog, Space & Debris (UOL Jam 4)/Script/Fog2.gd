d5cae7ed7fea9a4f7f56ee1eea0768f2abc75224
Lennart Zurmühl <lennart@lennart-thinkpad.fritz.box>
2021-04-18 16:22:13 +0200

    scheibenwischer

--- Fog, Space & Debris (UOL Jam 4)/Script/Fog2.gd
+++ Fog, Space & Debris (UOL Jam 4)/Script/Fog2.gd
@@ -4,22 +4,9 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var currentCooldown = 0
-const COOLDOWN = 10
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	modulate.a8 = 0
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if modulate.a8 < 255 && currentCooldown == 0:
-		modulate.a8 += 1
-		currentCooldown = COOLDOWN
-	else:
-		currentCooldown -= 1
 
 func clear():
-	print("clear")
-	modulate.a8 -= 125
+	$AnimatedSprite2.play("test")
+	$AnimatedSprite2.play("default")
+	$AnimatedSprite.clear()
---
+++ Fog, Space & Debris (UOL Jam 4)/Szenen/AnimatedSprite.gd
@@ -0,0 +1,25 @@
+extends AnimatedSprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var currentCooldown = 0
+const COOLDOWN = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	modulate.a8 = 0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if modulate.a8 < 255 && currentCooldown == 0:
+		modulate.a8 += 1
+		currentCooldown = COOLDOWN
+	else:
+		currentCooldown -= 1
+
+func clear():
+	modulate.a8 -= 125
+
