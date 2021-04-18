9cd894baff6013130891a9a3fde1f839b1ecea16
Lennart Zurmühl <lennart@lennart-thinkpad.fritz.box>
2021-04-18 14:17:51 +0200

    balancing

--- Fog, Space & Debris (UOL Jam 4)/Szenen/startup.gd
+++ Fog, Space & Debris (UOL Jam 4)/Szenen/startup.gd
@@ -1,19 +1,16 @@
-extends Node2D
+extends Node
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var speed = 0.15
+
 
 # Called when the node enters the scene tree for the first time.
-func _process(_delta):
-	position.y += speed
-	if (position.y >= 630):
-		position.y = 630
+func _ready():
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"),  -14)
+	get_tree().change_scene("res://Szenen/Menu.tscn") 
 
-func blowFog():
-	position.y -= 90
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
